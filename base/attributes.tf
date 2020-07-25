22211d8b37f32c01114f4f566e374bf33953a465
EC2 Default User <ec2-user@ip-172-31-2-44.ap-south-1.compute.internal>
2020-07-25 06:49:17 +0000

    added eip_association, sg

--- base/attributes.tf
+++ base/attributes.tf
@@ -17,3 +17,26 @@ resource "aws_s3_bucket" "s3_bucket" {
 output "s3_bucket_arn" {
     value = aws_s3_bucket.s3_bucket.arn
 }
+
+resource "aws_instance" "public_instance1" {
+    ami = "ami-08706cb5f68222d09"
+    instance_type = "t2.micro"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+    instance_id = "${aws_instance.public_instance1.id}"
+    allocation_id = "${aws_eip.lb.id}"
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "allow_tls"
+    description = "allow inbound 443 from eip"
+
+    ingress {
+    description = "TLS from EC2"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+    }
+}
